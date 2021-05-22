1a286480cd2131a9f5dd79acaa6f506859dcc61e
Gringowo <67298617+Gringowo@users.noreply.github.com>
2021-05-22 05:16:54 -0400

    programacion-web beta

--- Evaluacion_1_Boostrap/login_registro/validacion.js
+++ Evaluacion_1_Boostrap/login_registro/validacion.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     const formulario = document.getElementById('formulario');
     const inputs = document.querySelectorAll('#formulario input'); /*Accedemos a todos los inputs dentro del formulario en un arreglo. */
-    
-const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-} /*OBJETO QUE SE LLAMA "EXPPRESIONES" Y DENTRO TIENE VARIAS PROPIEDADES "usuario,nombre,password,etc" */
+
+    const expresiones = {
+        usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+        password: /^.{4,12}$/, // 4 a 12 digitos.
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    } /*OBJETO QUE SE LLAMA "EXPPRESIONES" Y DENTRO TIENE VARIAS PROPIEDADES "usuario,nombre,password,etc" */
 
     const email = document.getElementById("inputEmail4");
     const password = document.getElementById("inputPassword4");
@@ -18,7 +18,30 @@ const expresiones = {
     const postal = document.getElementById("inputZip");
     const form = document.getElementById('formulario');
     const parrafo = document.getElementById('warnings');
-    const alert = document.getElementById('alert');
+    /*EMAIL */
+    const alertemail = document.getElementById('alertemail');
+    const alertemail2 = document.getElementById('alertemail2');
+    const alertemail3 = document.getElementById('alertemail3');
+
+    /*PASSWORD ALERT */
+    const alertpassword = document.getElementById('alertpassword');
+    const alertpassword2 = document.getElementById('alertpassword2');
+    const alertpassword3 = document.getElementById('alertpassword3');
+
+    /*ADDRESS ALERT */
+    const alertaddress = document.getElementById('alertaddress');
+    const alertaddress2 = document.getElementById('alertaddress2');
+    const alertaddress3 = document.getElementById('alertaddress3');
+
+    /*CITY ALERT */
+    const alertcity = document.getElementById('alertcity');
+    const alertcity2 = document.getElementById('alertcity2');
+    const alertcity3 = document.getElementById('alertcity3');
+
+    /*POSTAL ALERT */
+    const alertpostal = document.getElementById('alertpostal');
+    const alertpostal2 = document.getElementById('alertpostal2');
+    const alertpostal3 = document.getElementById('alertpostal3');
     const alert2 = document.getElementById('alert2');
 
 
@@ -27,132 +50,130 @@ const expresiones = {
         e.preventDefault()
         let expReg = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
         if (!expReg.test(email.value)) {
-            alert2.classList.remove('d-none');
-            alert2.innerText = '*El correo no cumple con expresiones regulares';
-            console.log("EXPRESION REGULARRRRRRRRRRRRR")
-        } else{
-            alert2.classList.add('d-none');
+            alertemail.classList.remove('d-none');
+            alertemail.innerText = '*El correo no cumple con expresiones regulares';
+            console.log("hola correo")
+        } else {
+            console.log("ELSE CORREO")
+            alertemail.classList.add('d-none');
 
         }
         if (email.value.length < 6) {
             /*Correo corto */
-            console.log("sigo aqui jaja")
-            alert.classList.remove('d-none');
-            alert.innerText = '*El correo es menor a 6 caracteres';
+            alertemail2.classList.remove('d-none');
+            alertemail2.innerText = '*El correo es menor a 6 caracteres';
+        }else{
+            alertemail2.classList.add('d-none');
         }
-        if (email.value.length > 6) {
-            alert.classList.add('d-none');
-        }
-        if (email.value ==='') {
-            alert.classList.remove('d-none');
-            alert.innerText = '*Campo Correo Vacío';
+        if (email.value === '') {
+            alertemail3.classList.remove('d-none');
+            alertemail3.innerText = '*Campo Correo Vacío';
         }
         else {
-            console.log("else ")
+            alertemail3.classList.add('d-none');
         }
-            /*VALIDACION PASSWORD*/
-            let expRegpass = /^.{4,12}$/;
-            if (!expRegpass.test(password.value)) {
-                alert2.classList.remove('d-none');
-                alert2.innerText = '*La password no cumple con expresiones regulares';
-                console.log("EXPRESION REGULAR PASSWORD")
-            } else{
-                alert2.classList.add('d-none');
-    
-            }
-            if (email.value.length < 6) {
-                /*Correo corto */
-                alert.classList.remove('d-none');
-                alert.innerText = '*La password es menor a 6 caracteres'
-            }
-            if (email.value.length > 6) {
-                alert.classList.add('d-none');
-            }
-            if (email.value ==='') {
-                alert.classList.remove('d-none');
-                alert.innerText = '*Campo Password Vacío';
-            }
-            else {
-                console.log("else ")
-            }
+        /*VALIDACION PASSWORD*/
+        let expRegpass = /^.{4,12}$/;
+        if (!expRegpass.test(password.value)) {
+            alertpassword.classList.remove('d-none');
+            alertpassword.innerText = '*La password no cumple con expresiones regulares';
+            console.log("EXPRESION REGULAR PASSWORD")
+        } else {
+            alertpassword.classList.add('d-none');
 
-            /*VALIDACION DIRECCION */
-            let expRegdireccion = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
-            if (!expRegdireccion.test(address.value)) {
-                alert2.classList.remove('d-none');
-                alert2.innerText = '*La direccion no cumple con expresiones regulares';
-                console.log("EXPRESION REGULAR direccion")
-            } else{
-                alert2.classList.add('d-none');
-    
-            }
-            if (email.value.length < 6) {
-                /*Correo corto */
-                alert.classList.remove('d-none');
-                alert.innerText = '*La direccion es menor a 6 caracteres'
-            }
-            if (email.value.length > 6) {
-                alert.classList.add('d-none');
-            }
-            if (email.value ==='') {
-                alert.classList.remove('d-none');
-                alert.innerText = '*Campo Password Vacío';
-            }
-            else {
-                console.log("else ")
-            }
-            /*VALIDACION CIUDAD */
-            let expRegciudad = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
-            if (!expRegciudad.test(city.value)) {
-                alert2.classList.remove('d-none');
-                alert2.innerText = '*La Ciudad no cumple con expresiones regulares';
-                console.log("EXPRESION REGULAR ciudad")
-            } else{
-                alert2.classList.add('d-none');
-    
-            }
-            if (email.value.length < 6) {
-                /*Correo corto */
-                alert.classList.remove('d-none');
-                alert.innerText = '*La ciudad es menor a 6 caracteres'
-            }
-            if (email.value.length > 6) {
-                alert.classList.add('d-none');
-            }
-            if (email.value ==='') {
-                alert.classList.remove('d-none');
-                alert.innerText = '*Campo ciudad Vacío';
-            }
-            else {
-                console.log("else ")
-            }
-            /*VALIDACION CODIGO POSTAL */
-            let expRegcodigo = /^\d{7,14}$/;
-            if (!expRegcodigo.test(postal.value)) {
-                alert2.classList.remove('d-none');
-                alert2.innerText = '*El codigo postal no cumple con expresiones regulares';
-                console.log("EXPRESION REGULAR postal")
-            } else{
-                alert2.classList.add('d-none');
-    
-            }
-            if (email.value.length < 6) {
-                /*Correo corto */
-                alert.classList.remove('d-none');
-                alert.innerText = '*El codigo postal es menor a 6 caracteres'
-            }
-            if (email.value.length > 6) {
-                alert.classList.add('d-none');
-            }
-            if (email.value ==='') {
-                alert.classList.remove('d-none');
-                alert.innerText = '*Campo Codigo postal Vacío';
-            }
-            else {
-                console.log("else ")
-            }
+        }
+        if (password.value.length < 6) {
+            /*Correo corto */
+            alertpassword2.classList.remove('d-none');
+            alertpassword2.innerText = '*La password es menor a 6 caracteres'
+        }else{
+            alertpassword2.classList.add('d-none');
+        }
+        if (password.value === '') {
+            alertpassword3.classList.remove('d-none');
+            alertpassword3.innerText = '*Campo Password Vacío';
+        }
+        else {
+            alertpassword3.classList.add('d-none');
+            console.log("asddd")
+        }
+
+        /*VALIDACION DIRECCION */
+        let expRegdireccion = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+        if (!expRegdireccion.test(address.value)) {
+            alertaddress.classList.remove('d-none');
+            alertaddress.innerText = '*La direccion no cumple con expresiones regulares';
+            console.log("EXPRESION REGULAR direccion")
+        } else {
+            alertaddress.classList.add('d-none');
+
+        }
+        if (address.value.length < 6) {
+            /*Correo corto */
+            alertaddress2.classList.remove('d-none');
+            alertaddress2.innerText = '*La direccion es menor a 6 caracteres'
+        }
+        if (address.value.length > 6) {
+            alertaddress2.classList.add('d-none');
+        }
+        if (address.value === '') {
+            alertaddress3.classList.remove('d-none');
+            alertaddress3.innerText = '*Campo address Vacío';
+        }
+        else {
+            alertaddress3.classList.add('d-none');
+        }
+        /*VALIDACION CIUDAD */
+        let expRegciudad = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+        if (!expRegciudad.test(city.value)) {
+            alertcity.classList.remove('d-none');
+            alertcity.innerText = '*La Ciudad no cumple con expresiones regulares';
+            console.log("EXPRESION REGULAR ciudad")
+        } else {
+            alertcity.classList.add('d-none');
+
+        }
+        if (city.value.length < 6) {
+            /*Correo corto */
+            alertcity2.classList.remove('d-none');
+            alertcity2.innerText = '*La ciudad es menor a 6 caracteres'
+        }else{
+            alertcity2.classList.add('d-none');
+        }
+        if (city.value === '') {
+            alertcity3.classList.remove('d-none');
+            alertcity3.innerText = '*Campo ciudad Vacío';
+        }
+        else {
+            alertcity3.classList.add('d-none');
+        }
+        /*VALIDACION CODIGO POSTAL */
+        let expRegcodigo = /^\d{7,14}$/;
+        if (!expRegcodigo.test(postal.value)) {
+            alertpostal.classList.remove('d-none');
+            alertpostal.innerText = '*El codigo postal no cumple con expresiones regulares';
+            console.log("EXPRESION REGULAR postal")
+        } else {
+            alertpostal.classList.add('d-none');
+
+        }
+        if (postal.value.length < 6) {
+            /*Correo corto */
+            alertpostal2.classList.remove('d-none');
+            alertpostal2.innerText = '*El codigo postal es menor a 6 caracteres'
+        }else{
+            alertpostal2.classList.add('d-none');
+        }
+        
+        if (postal.value === '') {
+            alertpostal3.classList.remove('d-none');
+            alertpostal3.innerText = '*Campo Codigo postal Vacío';
+        }
+        else {
+            alertpostal3.classList.add('d-none');
+        }
     });
-    
+
 
 
 
