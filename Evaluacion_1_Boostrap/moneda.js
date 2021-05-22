d653236bbe02be8f9e2e32d3ad41fdaa5c153b09
Gringowo <67298617+Gringowo@users.noreply.github.com>
2021-05-22 05:11:06 -0400

    api v.02

---
+++ Evaluacion_1_Boostrap/moneda.js
@@ -0,0 +1,37 @@
+document.querySelector('#dolar').addEventListener('click', function(){
+    obtenerDatos();
+
+
+})
+
+function obtenerDatos(){
+    
+    let url = `https://mindicador.cl/api`;
+
+
+    const api = new XMLHttpRequest();
+    api.open('GET', url, true);
+    api.send();
+
+    api.onreadystatechange = function(){
+
+        if(this.status == 200 && this.readyState == 4){
+
+            let datos = JSON.parse(this.responseText);
+            console.log(datos.serie);
+            let resultado = document.querySelector('#resultado');
+            resultado.innerHTML = '';
+
+
+            for(let item of datos.serie){
+                resultado.innerHTML += `<li>${item.valor}</li>`
+                
+            }
+             
+        }
+    }
+
+
+
+     
+}
